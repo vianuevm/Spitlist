0381710381e37a0732b58ec4f2e3b67fc2a98768
Michael Vianueva <vianuevm@umich.edu>
2014-09-03 22:27:00 -0400

    Update config.py

--- config.py
+++ config.py
@@ -11,11 +11,3 @@ SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 CSRF_ENABLED = True
 SECRET_KEY = 'dogeIsMasterOfTheUniverse'
 
-UPLOAD_FOLDER = '/uploads'
-ALLOWED_EXTENSIONS = set(['cpp', 'h'])
-OPENID_PROVIDERS = [
-    { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
-    { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
-    { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
-    { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
-    { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
