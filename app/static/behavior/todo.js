c53a29ffbf904f16f2b3862e0986369c3da0204a
Mike Vianueva <vianuevm@umich.edu>
2014-09-06 17:12:11 -0400

    major frontend commit

---
+++ app/static/behavior/todo.js
@@ -0,0 +1,86 @@
+app = angular.module('TodoApp', ['ngRoute', 'mgo-mousetrap']);
+
+app.config(['$routeProvider', function($routeProvider){
+	$routeProvider
+		.when('/', {
+			templateUrl: '../static/main_todo.html',
+			controller: 'TodoController'
+		});
+}]);
+
+app.controller('TodoController', [
+	'$scope',
+	'$http',
+	function($scope, $http) {
+
+		$scope.newItem = "Starter";
+		$scope.todoList = [
+			{
+				'desc' : 'Pick up a panda',
+				'dueDate' : 'Yesterday'
+			}
+		];
+		$scope.colorCounter = 0;
+		$scope.changeColor = function() {
+			
+			if($scope.colorCounter === 0)
+			{
+				document.body.style.background = 'lightblue';
+				document.input = 'white';
+				$scope.colorCounter++;
+			} else if($scope.colorCounter === 1) {
+				document.body.style.background = '#333';
+				$scope.colorCounter++;
+			} else if($scope.colorCounter === 2) {
+				document.body.style.background = 'purple';
+				$scope.colorCounter++;
+			} else if($scope.colorCounter === 3) {
+				document.body.style.background = 'lightblue';
+				$scope.colorCounter++;
+			} else if($scope.colorCounter === 4) {
+				document.body.style.background = 'purple';
+				$scope.colorCounter++;
+			} else {
+				$scope.colorCounter = 0;
+				document.body.style.background = '#333';
+			}
+		};
+
+		$scope.addTodo = function() {
+			$scope.taskId++;
+			$scope.todoList.push(
+				{
+					'desc' : $scope.newItem,
+					'dueDate' : 'now'				}
+			);
+		};
+
+		$scope.deleteTodo = function(){
+			$scope.todoList.pop();
+		};
+
+		$scope.deleteSpecificTodo = function(todoList, index) {
+			todoList.splice(index, 1);
+		}
+
+	}]);
+
+app.directive('ngEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                	scope.todoList.push(
+					{
+						'desc' : scope.newItem,
+						'dueDate' : 'now'
+					});
+
+                });
+                event.preventDefault();
+            } else {
+            	console.log(event.which);
+            }
+        });
+    };
+});
