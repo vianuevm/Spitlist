98d4372f01eed7b1fa910c3302a3e30ba4eb7c27
Nina <ninazeng@umich.edu>
2014-09-06 17:50:12 -0400

    fixed return types for date functions. added conversion from python dates to javascript. added next_week funciton

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ import os
 import time
 from flask import render_template, redirect, session, url_for, request, send_from_directory, jsonify
 from app import app, db, models
-from datetime import datetime
+from datetime import datetime, timedelta
 from werkzeug import secure_filename
 
 @app.route('/',  methods = ['GET'])
@@ -50,7 +50,8 @@ def move_date():
 	task = models.Task.query.filter(models.Task.task_id == request.form['task_id'] ).first()
 	task.due_date = datetime.fromtimestamp(float(request.form['new_date']))
 	db.session.commit()
-	return jsonify(task=task.serialize())
+	javascript_time = time.mktime(task.due_date.timetuple()) * 1000
+	return str(javascript_time)
 
 @app.route('/delete', methods=['POST'])
 def delete():
@@ -65,3 +66,13 @@ def mark():
 	task.is_complete = bool(request.form["is_complete"])
 	db.session.commit()
 	return jsonify(success=True)
+
+@app.route('/next_week', methods=['POST'])
+def next_week():
+	task = models.Task.query.filter(models.Task.task_id == request.form['task_id'] ).first()
+	print "time before"
+	print task.due_date
+	task.due_date = datetime.fromtimestamp(float(request.form['due_date'])) + timedelta(days=7)
+	javascript_time = time.mktime(task.due_date.timetuple()) * 1000
+	db.session.commit()
+	return str(javascript_time)
