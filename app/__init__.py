4d2b3d7decc31308c83f9165462e9a1b066919d4
Nina <ninazeng@umich.edu>
2014-09-06 21:33:42 -0400

    added login framework

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,9 @@ from logging.handlers import RotatingFileHandler
 
 import os
 
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+
 from flask import Flask
 from config import basedir
 from flask_sqlalchemy import SQLAlchemy
@@ -14,5 +17,11 @@ db = SQLAlchemy(app)
 handler = RotatingFileHandler('log/application.log', maxBytes=10000, backupCount=1)
 handler.setLevel(logging.WARNING)
 app.logger.addHandler(handler)
+
+#begin login manager code
+lm = LoginManager()
+lm.init_app(app)
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
+lm.login_view = 'login'
 
 from app import views, models
